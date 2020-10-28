9b3360d78c39060152de19c2b9ed4d893e6973bf
Shilcof <robert.shilcof@gmail.com>
2020-10-28 12:20:57 +0000

    commit tree 26b7a31effc9d8fe8d30da56f201dae10045775d

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        self.knowledge = []
+    end
+
+    def learn(string)
+        knowledge << string
+    end
 
 end
